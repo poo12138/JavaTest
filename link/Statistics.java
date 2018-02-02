db43d6e691cd17c3cfae623ea18f9710b772d9dd
poo12138 <347547721@qq.com>
2018-02-02 10:19:02 +0800

    linkcode

--- link/Statistics.java
+++ link/Statistics.java
@@ -1,29 +1,50 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
 package link;
 
 public class Statistics {
+    public Statistics() {
+    }
 
     public int digitCounts(int k, int n) {
-        int count=0,singlecount=0;
-        int p=n;
-        int num=0;//表示处理数字的位数 num=0表示处理个位 1表示十位等
-        if(k==0) return (n/10)+1;
-        if(n==0) return 0;
-        while(p!=0){
-            if((p%10)>k) singlecount=(int) ((p/10+1)*(Math.pow(10, (double)num)));
-            if((p%10)==k) singlecount=(int) ((p/10)*(Math.pow(10, (double)num))+(n%((Math.pow(10, (double)num))))+1);
-            if((p%10)<k)  singlecount=(int) ((p/10)*(Math.pow(10, (double)num)));
-            count+=singlecount;
-            p=p/10;
-            num++;
+        int count = 0;
+        int singlecount = 0;
+        int p = n;
+        int num = 0;
+        if (k == 0) {
+            return n / 10 + 1;
+        } else if (n == 0) {
+            return 0;
+        } else {
+            while(p != 0) {
+                if (p % 10 > k) {
+                    singlecount = (int)((double)(p / 10 + 1) * Math.pow(10.0D, (double)num));
+                }
+
+                if (p % 10 == k) {
+                    singlecount = (int)((double)(p / 10) * Math.pow(10.0D, (double)num) + (double)n % Math.pow(10.0D, (double)num) + 1.0D);
+                }
+
+                if (p % 10 < k) {
+                    singlecount = (int)((double)(p / 10) * Math.pow(10.0D, (double)num));
+                }
+
+                count += singlecount;
+                p /= 10;
+                ++num;
+            }
+
+            return count;
         }
-        return count;
     }
-    public static void main(String[] args){
-        int a;
-        Statistics st=new Statistics();
-        a=st.digitCounts(1,20);
+
+    public static void main(String[] args) {
+        Statistics st = new Statistics();
+        int a = st.digitCounts(1, 20);
         System.out.println(a);
-
-
     }
 }
+
